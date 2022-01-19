62bdcf819de21c3d19585a06220724771b8dbb92
Alejandro Calle Saldarriaga <Alejandro.Calle434@gmail.com>
2022-01-18 21:37:46 -0500

    Update ejemplo_inicial.R

--- files/ejemplo_inicial.R
+++ files/ejemplo_inicial.R
@@ -5,7 +5,9 @@ library(ggplot2)
 iris <- read.csv("iris-write-from-docker.csv")
 petal_width <- iris$petal_width
 
+# calcular estadísticos
 mean(petal_width)
 sd(petal_width)
 
+# graficar
 ggplot(iris, aes(x=petal_width)) + geom_histogram()
